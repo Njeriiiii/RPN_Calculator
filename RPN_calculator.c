fce9ba1a6f67016db3847712bda14f0d90c1eb3a
Njeriiiii <144242109+Njeriiiii@users.noreply.github.com>
2024-04-04 09:31:50 +0300

    Add files via upload

---
+++ RPN_calculator.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+struct Stack {
+    int top;
+    unsigned capacity;
+    int* array;
+};
+
+struct Stack* createStack(unsigned capacity) {
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = (int*)malloc(stack->capacity * sizeof(int));
+    return stack;
+}
+
+int isFull(struct Stack* stack) {
+    return stack->top == stack->capacity - 1;
+}
+
+int isEmpty(struct Stack* stack) {
+    return stack->top == -1;
+}
+
+void push(struct Stack* stack, int item) {
+    if (isFull(stack))
+        return;
+    stack->array[++stack->top] = item;
+}
+
+int pop(struct Stack* stack) {
+    if (isEmpty(stack))
+        return -1;
+    return stack->array[stack->top--];
+}
+
+int peek(struct Stack* stack) {
+    if (isEmpty(stack))
+        return -1;
+    return stack->array[stack->top];
+}
+
+int precedence(char op) {
+    switch(op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+        case '%':
+            return 2;
+        default:
+            return -1;
+    }
+}
+
+void infixPostfix(char* exp) {
+    int i, k;
+    struct Stack* stack = createStack(strlen(exp));
+    if(!stack)
+        return;
+
+    for (i = 0, k = -1; exp[i]; ++i) {
+        if (isdigit(exp[i]))
+            exp[++k] = exp[i];
+        else if (exp[i] == '(')
+            push(stack, exp[i]);
+        else if (exp[i] == ')') {
+            while (!isEmpty(stack) && peek(stack) != '(')
+                exp[++k] = pop(stack);
+            if (!isEmpty(stack) && peek(stack) != '(')
+                return;
+            else
+                pop(stack);
+        } else {
+            while (!isEmpty(stack) && precedence(exp[i]) <= precedence(peek(stack)))
+                exp[++k] = pop(stack);
+            push(stack, exp[i]);
+        }
+    }
+
+    while (!isEmpty(stack))
+        exp[++k] = pop(stack);
+
+    exp[++k] = '\0';
+}
+
+int main() {
+    char exp[100];
+    printf("Enter the infix expression: ");
+    scanf("%s", exp);
+
+    infixPostfix(exp);
+    printf("Postfix expression: %s\n", exp);
+    return 0;
+}
